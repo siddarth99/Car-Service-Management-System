fa9deba09cbf624c83861145ac2ea4b6cda1831f
siddarth99 <38921750+siddarth99@users.noreply.github.com>
2020-05-19 23:44:48 +0530

    Add files via upload

--- CS_Management.cpp
+++ CS_Management.cpp
@@ -1,46 +1,85 @@
-// CS_Management.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include<fstream>
+#include<chrono>
+#include<ctime>
+#include<string>
+#include<Windows.h>
+
+
 using namespace std;
 
 class CarService {
-    void  CheckIn();
-    void CheckOut();
-    void Admin();
-    void Employee();
-    void AlreadyCust();
-    void NewCust();
-    void RTHP();
-    void NewComplain();
-    void ServiceHistory();
-    void Submit();
+public:
+	void CheckIn();
+	//void CheckOut();
+	//void Admin();
+	//void Employee();
+	void AlreadyCust();
+	void NewCust();
+	//void RTHP();
+	//void NewComplain();
+	//void ServiceHistory();
+	//void Submit();
+	void Issues(string);
 
 };
 void CarService::CheckIn() {
-    
-    cout << "1. Already Customer\n2. New Customer\n3. Return to Home Page\n";
-    cout << "Enter your choice: ";
-    int choice;
-    cin >> choice;
-    switch(choice)
-    {
-    case 1: AlreadyCust();
-        break;
-    case 2: 
 
-int main()
-{
-    
+	cout << "1. Already Customer\n2. New Customer\n3. Return to Home Page\n";
+	cout << "Enter your choice: ";
+	int choice;
+	cin >> choice;
+	switch (choice)
+	{
+	case 1: AlreadyCust();
+		break;
+	case 2: NewCust();
+		break;
+	//case 3: RTHP();
+		//break;
+	}
+}
+void CarService::AlreadyCust() {
+
+}
+void CarService::NewCust() {
+	ofstream CustomerFile("CustomerDetails.txt",ios::app);
+	string name;
+	long phNumber;
+	string vehicleNo;
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Enter your Phone Number: ";
+	cin >> phNumber;
+	cout << "Enter your Vehicle Number: ";
+	cin >> vehicleNo;
+	
+	CustomerFile << name << " " << phNumber << " " << vehicleNo << endl;
+	CustomerFile.close();
+	Issues(vehicleNo);
+
+}
+void CarService::Issues(string vehicle_numb) {
+	string issus;
+	SYSTEMTIME st;
+	GetSystemTime(&st);
+
+
+	ofstream CarIssues("IssuesDetails.txt");
+	cout << "Enter the Issues in your Vehicle:";
+	cin.ignore();
+	getline(cin, issus);
+	CarIssues <<st.wDay << "/"<<st.wMonth<<"/"<<st.wYear<<" "<<st.wHour<<":"<<st.wMinute <<" "<< vehicle_numb << " " << issus << endl;
+	CarIssues.close();
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+		int main()
+		{
+			CarService cs;
+			cs.CheckIn();
+		
+
+		}
