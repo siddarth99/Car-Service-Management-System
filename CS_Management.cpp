08002e81a83137492be5a9a7737b9315581b493f
siddarth99 <38921750+siddarth99@users.noreply.github.com>
2020-05-20 11:09:38 +0530

    Add files via upload

--- CS_Management.cpp
+++ CS_Management.cpp
@@ -4,7 +4,7 @@
 #include<ctime>
 #include<string>
 #include<Windows.h>
-
+#include<functional>
 
 using namespace std;
 
@@ -12,7 +12,7 @@ class CarService {
 public:
 	void CheckIn();
 	//void CheckOut();
-	//void Admin();
+	void Admin();
 	//void Employee();
 	void AlreadyCust();
 	void NewCust();
@@ -54,13 +54,14 @@ void CarService::NewCust() {
 	cout << "Enter your Vehicle Number: ";
 	cin >> vehicleNo;
 	
-	CustomerFile << name << " " << phNumber << " " << vehicleNo << endl;
+	CustomerFile << name << " " << phNumber <<" "<< vehicleNo << endl;
 	CustomerFile.close();
 	Issues(vehicleNo);
 
+
 }
 void CarService::Issues(string vehicle_numb) {
-	string issus;
+	string issues;
 	SYSTEMTIME st;
 	GetSystemTime(&st);
 
@@ -68,18 +69,59 @@ void CarService::Issues(string vehicle_numb) {
 	ofstream CarIssues("IssuesDetails.txt");
 	cout << "Enter the Issues in your Vehicle:";
 	cin.ignore();
-	getline(cin, issus);
-	CarIssues <<st.wDay << "/"<<st.wMonth<<"/"<<st.wYear<<" "<<st.wHour<<":"<<st.wMinute <<" "<< vehicle_numb << " " << issus << endl;
+	getline(cin, issues);
+	CarIssues <<st.wDay << "/"<<st.wMonth<<"/"<<st.wYear<<" "<<st.wHour<<":"<<st.wMinute <<" "<< vehicle_numb << " " << issues << endl;
 	CarIssues.close();
 
 
 }
+void CarService::Admin() {
+	fstream AdminFile;
+	AdminFile.open("Admin.txt",ios::in);
+	string userName, password, buffer, passw;
+	cout << "Enter Username: ";
+	cin >> userName;
+	cout << "Enter Password: ";
+	HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	DWORD mode = 0;
+	GetConsoleMode(hStdin, &mode);
+	SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
+	cin.ignore();
+	getline(cin, password);
+	SetConsoleMode(hStdin, mode);
+	hash<string> mystdhash;
+	password = mystdhash(password);
+	if (!AdminFile.is_open()) {
+		AdminFile.open("Admin.txt", ios::out);
+		AdminFile << userName << "|" << password << "$";
+		cout << userName << password;
 
+	}
+	else {
+
+		buffer.erase();
+		int i = 0;
+		getline(AdminFile, buffer);
+		while (buffer[i] != '|')
+			userName += buffer[i++];
+		i++;
+		while (buffer[i] != '$')
+			passw += buffer[i++];
+		if (passw == password)
+		{
+			cout << "login Successfull";
+		}
+		else {
+			cout << "login not successfull";
+			Admin();
+		}
+	}
+}
 
 		int main()
 		{
 			CarService cs;
-			cs.CheckIn();
+			cs.Admin();
 		
 
 		}
